3e25b6a88ec99abfba42375d59135986ca9a8027
Indigo9898 <33042992+Indigo9898@users.noreply.github.com>
2023-05-02 21:59:36 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     minHeight: {
